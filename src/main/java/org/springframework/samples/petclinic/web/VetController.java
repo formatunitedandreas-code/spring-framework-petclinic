2e8ccba94f093590e5f4c318b099aff4631b8dd6
Elvis <formatunitedandreas@gmail.com>
2026-07-12 16:48:46 +0200

    Refactor PetClinic vet controller literals

--- src/main/java/org/springframework/samples/petclinic/web/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VetController.java
@@ -33,6 +33,8 @@ import java.util.Map;
 @Controller
 public class VetController {
 
+    private static final String MODEL_ATTRIBUTE_VETS = "vets";
+    private static final String VIEW_VET_LIST = "vets/vetList";
     private final ClinicService clinicService;
 
     public VetController(ClinicService clinicService) {
@@ -43,8 +45,8 @@ public class VetController {
     public String showVetList(Map<String, Object> model) {
         // Here we are returning an object of type 'Vets' rather than a collection of Vet objects
         // so it is simpler for Object-Xml mapping
-        model.put("vets", getVets());
-        return "vets/vetList";
+        model.put(MODEL_ATTRIBUTE_VETS, getVets());
+        return VIEW_VET_LIST;
     }
 
     @GetMapping(value = "/vets.json", produces = MediaType.APPLICATION_JSON_VALUE)
