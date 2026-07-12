b3234871ddf0a56999c35675147f5b261dcbfec8
Elvis <formatunitedandreas@gmail.com>
2026-07-12 09:34:55 +0200

    Refactor PetClinic service method spacing

--- src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
@@ -75,7 +75,6 @@ public class ClinicServiceImpl implements ClinicService {
         ownerRepository.save(owner);
     }
 
-
     @Override
     @Transactional
     public void saveVisit(Visit visit) {
