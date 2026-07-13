530608d3ff633e2e2a6453722bee8f433ffb7a91
Elvis <formatunitedandreas@gmail.com>
2026-07-13 22:04:53 +0200

    Refactor PetClinic simplify owner detail ModelAndView

--- src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
@@ -121,9 +121,8 @@ public class OwnerController {
      */
     @GetMapping("/owners/{ownerId}")
     public ModelAndView showOwner(@PathVariable("ownerId") int ownerId) {
-        ModelAndView mav = new ModelAndView("owners/ownerDetails");
-        mav.addObject(this.clinicService.findOwnerById(ownerId));
-        return mav;
+        return new ModelAndView("owners/ownerDetails")
+            .addObject(this.clinicService.findOwnerById(ownerId));
     }
 
 }
