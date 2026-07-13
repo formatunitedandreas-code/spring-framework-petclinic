963fbc44a6278c4bb5c7a9a7250b9d2a40fe8fe7
Elvis <formatunitedandreas@gmail.com>
2026-07-13 12:10:53 +0200

    Refactor PetClinic owner lastName query formatting

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
@@ -52,7 +52,8 @@ public class JpaOwnerRepositoryImpl implements OwnerRepository {
         // using 'join fetch' because a single query should load both owners and pets
         // using 'left join fetch' because it might happen that an owner does not have pets yet
         return this.em.createQuery(
-            "SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.lastName LIKE :lastName", Owner.class)
+            "SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets " +
+            "WHERE owner.lastName LIKE :lastName", Owner.class)
             .setParameter("lastName", lastName + "%")
             .getResultList();
     }
