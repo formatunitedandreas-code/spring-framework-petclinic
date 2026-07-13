619e4a3ea2d8ccaf7809eb2a45a8506125633d55
Elvis <formatunitedandreas@gmail.com>
2026-07-13 11:40:37 +0200

    Refactor PetClinic owner pets sorting helper

--- src/main/java/org/springframework/samples/petclinic/model/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/model/Owner.java
@@ -96,6 +96,10 @@ public class Owner extends Person {
     }
 
     public List<Pet> getPets() {
+        return sortedPets();
+    }
+
+    private List<Pet> sortedPets() {
         List<Pet> sortedPets = new ArrayList<>(getPetsInternal());
         sortedPets.sort(Comparator.comparing(Pet::getName, String.CASE_INSENSITIVE_ORDER));
         return Collections.unmodifiableList(sortedPets);
