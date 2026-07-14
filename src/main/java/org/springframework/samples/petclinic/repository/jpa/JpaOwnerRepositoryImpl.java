2cb6fdee111694922a740ce07b33606830f5a38c
Elvis <formatunitedandreas@gmail.com>
2026-07-14 09:17:48 +0200

    Refactor PetClinic Extract inline SQL literal from repository method 'fin

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
@@ -37,6 +37,9 @@ public class JpaOwnerRepositoryImpl implements OwnerRepository {
 
     private static final String FIND_BY_ID_SQL = "SELECT owner FROM Owner owner left join fetch owner.pets WHERE owner.id = :id";
 
+    private static final String FIND_BY_LAST_NAME_SQL = "SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets " +
+            "WHERE owner.lastName LIKE :lastName";
+
     private final EntityManager em;
 
     public JpaOwnerRepositoryImpl(EntityManager em) {
@@ -54,8 +57,7 @@ public class JpaOwnerRepositoryImpl implements OwnerRepository {
         // using 'join fetch' because a single query should load both owners and pets
         // using 'left join fetch' because it might happen that an owner does not have pets yet
         return this.em.createQuery(
-            "SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets " +
-            "WHERE owner.lastName LIKE :lastName", Owner.class)
+            FIND_BY_LAST_NAME_SQL, Owner.class)
             .setParameter("lastName", lastName + "%")
             .getResultList();
     }
