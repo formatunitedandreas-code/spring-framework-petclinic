0b2592bdcf9c8e648524db27842a9716aec913de
Elvis <formatunitedandreas@gmail.com>
2026-07-15 17:41:02 +0200

    Refactor PetClinic Wrap a long constant or method signature readability l

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPetRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPetRepositoryImpl.java
@@ -47,7 +47,8 @@ public class JdbcPetRepositoryImpl implements PetRepository {
     private static final String ID = "id";
 
 
-    private static final String FIND_OWNER_ID_BY_PET_ID_SQL = "SELECT owner_id FROM pets WHERE id=:id";
+    private static final String FIND_OWNER_ID_BY_PET_ID_SQL = "SELECT owner_id FROM pets WHERE " +
+        "id=:id";
 
     private static final String FIND_PET_TYPES_SQL = "SELECT id, name FROM types ORDER BY name";
 
