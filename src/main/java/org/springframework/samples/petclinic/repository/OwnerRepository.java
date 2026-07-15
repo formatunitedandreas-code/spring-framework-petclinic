e6493c34167ff50e281a1477fc4cb22b83669f79
Elvis <formatunitedandreas@gmail.com>
2026-07-15 10:45:16 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/repository/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/OwnerRepository.java
@@ -21,7 +21,8 @@ import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.model.Owner;
 
 /**
- * Repository class for <code>Owner</code> domain objects All method names are compliant with Spring Data naming
+ * Repository class for <code>Owner</code> domain objects All method names are compliant with
+ * Spring Data naming
  * conventions so this interface can easily be extended for Spring Data See here:
  * http://static.springsource.org/spring-data/jpa/docs/current/reference/html/
  * jpa.repositories.html#jpa.query-methods.query-creation
