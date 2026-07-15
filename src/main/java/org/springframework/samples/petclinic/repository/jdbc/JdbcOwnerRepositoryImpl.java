cc1a64817a5427dcd53d04d2cf62955b2e5f31fd
Elvis <formatunitedandreas@gmail.com>
2026-07-15 07:37:17 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryImpl.java
@@ -163,7 +163,8 @@ public class JdbcOwnerRepositoryImpl implements OwnerRepository {
     }
 
     /**
-     * Loads the {@link Pet} and {@link Visit} data for the supplied {@link List} of {@link Owner Owners}.
+     * Loads the {@link Pet} and {@link Visit} data for the supplied {@link List} of
+     * {@link Owner Owners}.
      *
      * @param owners the list of owners for whom the pet and visit data should be loaded
      * @see #loadPetsAndVisits(Owner)
