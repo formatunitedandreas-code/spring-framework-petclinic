e173690d47a3b16c8753ea7797ee8219959fb999
Elvis <formatunitedandreas@gmail.com>
2026-07-15 10:11:56 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
@@ -50,7 +50,8 @@ public class JpaOwnerRepositoryImpl implements OwnerRepository {
     /**
      * Important: in the current version of this method, we load Owners with all their Pets
      * and Visits while
-     * we do not need Visits at all, and we only need one property from the Pet objects (the 'name' property).
+     * we do not need Visits at all, and we only need one property from the Pet objects (the
+     * 'name' property).
      * There are some ways to improve it such as:
      * - creating a Lightweight class
      * - Turning on lazy-loading and using the open session in view pattern
