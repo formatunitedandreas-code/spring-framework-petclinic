4f2ab0469f919b652fd90ed283d40ce1c7b85329
Elvis <formatunitedandreas@gmail.com>
2026-07-15 18:20:50 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
@@ -20,7 +20,8 @@ import jakarta.persistence.MappedSuperclass;
 
 
 /**
- * Simple JavaBean domain object adds a name property to <code>BaseEntity</code>. Used as a base class for objects
+ * Simple JavaBean domain object adds a name property to <code>BaseEntity</code>. Used as a base
+ * class for objects
  * needing these properties.
  *
  * @author Ken Krebs
