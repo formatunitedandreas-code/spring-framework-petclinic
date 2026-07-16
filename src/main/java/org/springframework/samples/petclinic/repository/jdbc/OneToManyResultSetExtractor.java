0e03a3c59d69b72d0136f2d45c2743db1d3562f8
Elvis <formatunitedandreas@gmail.com>
2026-07-17 00:18:41 +0200

    Refactor PetClinic Normalize a contiguous leading-tab indentation block t

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/OneToManyResultSetExtractor.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/OneToManyResultSetExtractor.java
@@ -68,7 +68,7 @@ public abstract class OneToManyResultSetExtractor<R, C, K> implements ResultSetE
     protected final RowMapper<R> rootMapper;
     protected final RowMapper<C> childMapper;
 
-	/**
+    /**
 	 * Creates a new {@link OneToManyResultSetExtractor} from the given {@link RowMapper}s.
 	 *
 	 * @param rootMapper {@link RowMapper} to map the root entity, must not be {@literal null}.
