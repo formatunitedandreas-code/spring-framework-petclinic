ce546f75308620e61c69f2a739157fe118660015
Elvis <formatunitedandreas@gmail.com>
2026-07-17 01:53:09 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/OneToManyResultSetExtractor.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/OneToManyResultSetExtractor.java
@@ -75,7 +75,8 @@ public abstract class OneToManyResultSetExtractor<R, C, K> implements ResultSetE
 	 * Creates a new {@link OneToManyResultSetExtractor} from the given
 	 * {@link RowMapper}s.
 	 *
-	 * @param rootMapper {@link RowMapper} to map the root entity, must not be {@literal null}.
+	 * @param rootMapper {@link RowMapper} to map the root entity, must not be
+	 * {@literal null}.
 	 * @param childMapper {@link RowMapper} to map the root entities, must not be {@literal null}.
 	 */
     protected OneToManyResultSetExtractor(RowMapper<R> rootMapper, RowMapper<C> childMapper) {
