480dc2e98b9cfc885dcf2db0fcb420da73bcc3ca
Elvis <formatunitedandreas@gmail.com>
2026-07-17 07:48:27 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
+++ src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
@@ -39,7 +39,8 @@ public class EntityUtils {
     }
 
     /**
-     * Look up the entity of the given class with the given id in the given collection.
+     * Look up the entity of the given class with the given id in the
+     * given collection.
      *
      * @param entities    the collection to search
      * @param entityClass the entity class to look up
