90e048498fb33ded142020348557193bc9275ac3
Elvis <formatunitedandreas@gmail.com>
2026-07-17 02:02:10 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
@@ -48,7 +48,8 @@ public class JpaOwnerRepositoryImpl implements OwnerRepository {
     }
 
     /**
-     * Important: in the current version of this method, we load Owners with all their Pets
+     * Important: in the current version of this method, we load Owners
+     * with all their Pets
      * and Visits while
      * we do not need Visits at all, and we only need one property from the Pet objects (the
      * 'name' property).
