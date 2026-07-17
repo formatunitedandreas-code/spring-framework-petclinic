d3de2a2ac4aae75d3868ee0462cdbfcb50d9bf1f
formatunitedandreas-code <formatunitedandreas@gmail.com>
2026-07-17 07:52:55 +0200

    Refactor PetClinic autonomous wave 150
    PR #102
    branch threshold-governed-refactor-demo-150
    local validation: Maven test BUILD SUCCESS
    CI: all visible checks passed
    non-claims: no upstream interaction, no release, no deploy, no public readiness/correctness/security/compliance claim

--- src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
+++ src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
@@ -22,7 +22,8 @@ import org.springframework.orm.ObjectRetrievalFailureException;
 import org.springframework.samples.petclinic.model.BaseEntity;
 
 /**
- * Utility methods for handling entities. Separate from the BaseEntity class mainly because of
+ * Utility methods for handling entities. Separate from the BaseEntity class
+ * mainly because of
  * dependency on the
  * ORM-associated ObjectRetrievalFailureException.
  *
@@ -38,7 +39,8 @@ public class EntityUtils {
     }
 
     /**
-     * Look up the entity of the given class with the given id in the given collection.
+     * Look up the entity of the given class with the given id in the
+     * given collection.
      *
      * @param entities    the collection to search
      * @param entityClass the entity class to look up
--- src/main/java/org/springframework/samples/petclinic/web/PetValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/PetValidator.java
@@ -24,7 +24,8 @@ import org.springframework.validation.Validator;
 /**
  * <code>Validator</code> for <code>Pet</code> forms.
  * <p>
- * We're not using Bean Validation annotations here because it is easier to define such
+ * We're not using Bean Validation annotations here because it is
+ * easier to define such
  * validation rule in Java.
  * </p>
  *
