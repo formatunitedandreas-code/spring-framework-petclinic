dd2579895d3ca37a1e695ffe956c837c4f6a6f1f
Elvis <formatunitedandreas@gmail.com>
2026-07-17 07:30:23 +0200

    Refactor PetClinic Wrap one long line comment without changing source beh

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
@@ -73,7 +73,8 @@ public class JpaOwnerRepositoryImpl implements OwnerRepository {
     public Owner findById(int id) {
         // using 'join fetch' because a single query should load
         // both owners and pets
-        // using 'left join fetch' because it might happen that an owner does not have pets yet
+        // using 'left join fetch' because it might happen that an owner does
+        // not have pets yet
         return this.em.createQuery(
             FIND_BY_ID_SQL, Owner.class)
             .setParameter("id", id)
