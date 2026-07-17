79a1eaab08a8574ed20bf6a18775f86d939cd3e0
formatunitedandreas-code <formatunitedandreas@gmail.com>
2026-07-17 07:37:25 +0200

    Refactor PetClinic autonomous wave 148
    PR #101
    branch threshold-governed-refactor-demo-148
    local validation: Maven test BUILD SUCCESS
    CI: all visible checks passed
    non-claims: no upstream interaction, no release, no deploy, no public readiness/correctness/security/compliance claim

--- src/main/java/org/springframework/samples/petclinic/model/Vets.java
+++ src/main/java/org/springframework/samples/petclinic/model/Vets.java
@@ -22,7 +22,8 @@ import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 /**
- * Simple domain object representing a list of veterinarians. Mostly here to be used for
+ * Simple domain object representing a list of veterinarians. Mostly
+ * here to be used for
  * the 'vets' {@link
  * org.springframework.web.servlet.view.xml.MarshallingView}.
  *
