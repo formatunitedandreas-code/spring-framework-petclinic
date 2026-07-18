601987f76c73bea426fcd46b9bb4ee3591e86a2f
Elvis <formatunitedandreas@gmail.com>
2026-07-19 00:05:24 +0200

    Refactor PetClinic Wrap a long constant or method signature readability l

--- src/main/java/org/springframework/samples/petclinic/web/PetValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/PetValidator.java
@@ -54,7 +54,10 @@ public class PetValidator implements Validator {
         }
     }
 
-    private void validateRequiredFieldsForNewPet(Errors errors, Pet pet) {
+    private void validateRequiredFieldsForNewPet(
+        Errors errors,
+        Pet pet
+    ) {
         if (pet.isNew() && pet.getType() == null) {
             rejectRequiredField(errors, FIELD_TYPE);
         }
