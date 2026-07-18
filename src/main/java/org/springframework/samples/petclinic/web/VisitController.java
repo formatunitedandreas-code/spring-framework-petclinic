f7bee5159a3233c508b96eec233adee6c93b3e0b
Elvis <formatunitedandreas@gmail.com>
2026-07-18 23:40:25 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/web/VisitController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VisitController.java
@@ -58,7 +58,8 @@ public class VisitController {
      * annotated method.
      * 2 goals:
      * - Make sure we always have fresh data
-     * - Since we do not use the session scope, make sure that Pet object
+     * - Since we do not use the session scope, make
+     * sure that Pet object
      * always has an id
      * (Even though id is not part of the form fields)
      *
