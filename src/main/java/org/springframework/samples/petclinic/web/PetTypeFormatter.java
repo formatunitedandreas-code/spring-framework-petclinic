0def5df973301c7849ef004152b4d8082dc8609f
Elvis <formatunitedandreas@gmail.com>
2026-07-18 23:35:39 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/web/PetTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/web/PetTypeFormatter.java
@@ -31,7 +31,8 @@ import org.springframework.samples.petclinic.service.ClinicService;
  * of type 'PetType'.
  * Starting from Spring
  * 3.0, Formatters have
- * come as an improvement in comparison to legacy PropertyEditors. See the
+ * come as an improvement in comparison to legacy
+ * PropertyEditors. See the
  * following links for
  * more details: - The
  * Spring ref doc: http://static.springsource.org/spring/docs/current/spring-framework-reference/html/
