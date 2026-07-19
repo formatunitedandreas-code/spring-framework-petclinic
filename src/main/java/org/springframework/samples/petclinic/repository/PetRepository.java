7811b7c6f88c7a5f48b28bfc1516b33f0a89afce
Elvis <formatunitedandreas@gmail.com>
2026-07-19 18:13:53 +0200

    Refactor PetClinic Wrap one long Javadoc comment line without changing so

--- src/main/java/org/springframework/samples/petclinic/repository/PetRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/PetRepository.java
@@ -23,7 +23,8 @@ import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.PetType;
 
 /**
- * Repository class for <code>Pet</code> domain objects All method names
+ * Repository class for <code>Pet</code> domain objects
+ * All method names
  * are compliant with
  * Spring Data naming
  * conventions so this interface can easily be extended for
